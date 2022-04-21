9b240ade38bc5b782ab58afb4cb6c24c2a51e84a
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-21 17:53:22 +0400

    first commit

---
+++ Inheritance_Access modifiers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Inheritance_Access_modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+        }
+        class Admin:User
+        {
+            
+            public bool issuperadmin;
+            public string section;
+            public Admin(string username, int pasword, bool Issuperadmin, string Secton):base(username,pasword)
+            {
+
+                issuperadmin = Issuperadmin;
+                section = Secton;
+            }
+
+
+
+
+
+
+
+
+        }
+
+        class User
+        {
+
+            private string _Username;
+            private int _Pasword;
+            public User(string username, int pasword)
+            {
+                _Username = username;
+                _Pasword = pasword;
+
+            }
+            public string Username_pasword
+            {
+
+                get
+                {
+
+                    return _Username;
+
+
+                }
+                set
+                {
+
+                }
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
